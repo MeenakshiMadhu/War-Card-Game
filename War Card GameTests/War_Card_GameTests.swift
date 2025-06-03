ff4fd2f7852917488484d998183091a34a6f2e7b
Meenakshi Madhu <meenakshimadhu00@gmail.com>
2025-06-03 14:12:06 -0400

    Initial Commit

---
+++ War Card GameTests/War_Card_GameTests.swift
@@ -0,0 +1,16 @@
+//
+//  War_Card_GameTests.swift
+//  War Card GameTests
+//
+//  Created by Meenakshi Madhu on 6/3/25.
+//
+
+import Testing
+
+struct War_Card_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
